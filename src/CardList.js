cc2cb2c71d7b541bf093cf9731f1c9fc9f12ce0d
Fortune Ekeruo <legalmody@gmail.com>
2020-12-23 01:08:33 +0100

    add a root App component

--- src/CardList.js
+++ src/CardList.js
@@ -3,15 +3,15 @@ import Card from './Card';
 
 const CardList = ({ robots }) => (
   <>
-    {robots.map((robot) =>
+    {robots.map((robot) => (
       <Card
         key={robot.id}
         id={robot.id}
         name={robot.name}
         email={robot.email}
       />
-    )}
+    ))}
   </>
-)
+);
 
 export default CardList;
