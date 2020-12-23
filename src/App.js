cc8e1eff5cfa59cff04d0675099d09e3f8e8fea4
Fortune Ekeruo <legalmody@gmail.com>
2020-12-23 01:17:07 +0100

    add searchbox component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import { robots } from './robots';
 
 class App extends Component {
   render () {
     return (
-      <div>
+      <div className="tc">
         <h1>Robo friends</h1>
+        <SearchBox />
         <CardList robots={robots}/>
       </div>
     )
---
+++ src/SearchBox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchBox = () => (
+  <div className="pa2">
+    <input
+      type="search"
+      className="pa3 ba b--green"
+      placeholder="Search robo friends"
+    />
+  </div>
+)
+
+export default SearchBox;
