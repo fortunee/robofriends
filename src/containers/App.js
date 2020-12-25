48c4a86d77a54c54585a5f6846b3bf7001f67cd1
Fortune Ekeruo <legalmody@gmail.com>
2020-12-25 05:07:26 +0100

    replace robots state with reducer  robots

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
@@ -6,25 +6,28 @@ import Loading from '../components/Loading';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from './ErrorBoundary';
 
-import { setSearchField } from '../actions';
+import { requestRobots, setSearchField } from '../actions';
 
 import './App.css';
 
 const App = (props) => {
-  const [robots, setRobots] = useState([]);
-  const { searchField, onSearchChange } = props;
+  const {
+    searchField,
+    onSearchChange,
+    onRequestRobots,
+    robots,
+    isPending,
+  } = props;
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((res) => res.json())
-      .then((data) => setRobots(data));
-  }, []);
+    onRequestRobots();
+  }, [onRequestRobots]);
 
   const filteredRobots = robots.filter((rob) =>
     rob.name.toLocaleLowerCase().includes(searchField.toLocaleLowerCase())
   );
 
-  return !robots.length ? (
+  return isPending ? (
     <Loading />
   ) : (
     <div className="tc">
@@ -41,10 +44,14 @@ const App = (props) => {
 
 const mapStateToProps = (state) => ({
   searchField: state.searchRobots.searchField,
+  isPending: state.requestRobots.isPending,
+  robots: state.requestRobots.robots,
+  error: state.requestRobots.error,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   onSearchChange: (e) => dispatch(setSearchField(e.target.value)),
+  onRequestRobots: (e) => dispatch(requestRobots()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
