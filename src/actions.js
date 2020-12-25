1595f4920e599dd180bad1741e26361cd0139f9f
Fortune Ekeruo <legalmody@gmail.com>
2020-12-25 04:25:00 +0100

    add request robots action

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,23 @@
-import { CHANGE_SEARCH_FIELD } from './contants';
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED,
+} from './contants';
 
 export const setSearchField = (text) => ({
   type: CHANGE_SEARCH_FIELD,
   payload: text,
 });
+
+export const requestRobots = async (dispatch) => {
+  dispatch({ type: REQUEST_ROBOTS_PENDING });
+  try {
+    const data = await fetch(
+      'https://jsonplaceholder.typicode.com/users'
+    ).then((res) => res.json());
+    dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error });
+  }
+};
