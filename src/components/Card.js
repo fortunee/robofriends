af5a09b01cdba29c14be42a040871635b64c430b
Fortune Ekeruo <legalmody@gmail.com>
2020-12-28 18:04:33 +0100

    use correct img sizes

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ id, name, email }) => (
   <div className="tc bg-light-blue dib br3 pa3 ma2 grow shadow-5">
-    <img src={`https://robohash.org/${id}?200x200`} alt="robots"/>
+    <img src={`https://robohash.org/${id}?size=200x200`} alt="robots"/>
     <div>
       <h2>{name}</h2>
       <p>{email}</p>
