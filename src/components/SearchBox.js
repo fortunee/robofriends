7e4059c8e46d6c47af0ac2cc9c5b714fec4baf95
Fortune Ekeruo <legalmody@gmail.com>
2020-12-28 17:58:02 +0100

    add associated label to search input for better browseer assesibility

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,6 +3,7 @@ import React from 'react';
 const SearchBox = ({ searchChange }) => (
   <div className="pa2">
     <input
+      aria-label="Search Robos"
       type="search"
       className="pa3 ba b--green"
       placeholder="Search robo friends"
