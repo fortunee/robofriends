6d3d63a75803345ce6432377027fb07c8035038d
Fortune Ekeruo <legalmody@gmail.com>
2020-12-29 02:02:03 +0100

    cleanup error boundary component test

--- src/components/ErrorBoundary.spec.js
+++ src/components/ErrorBoundary.spec.js
@@ -1,30 +1,28 @@
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
 
 import ErrorBoundary from './ErrorBoundary';
 
 describe('<ErrorBoundary />', () => {
-  const component = shallow(<ErrorBoundary />);
-
   it('Should render a ErrorBoundary component', () => {
+    const component = shallow(<ErrorBoundary />);
     expect(component.debug()).toMatchSnapshot();
   });
-  
+
   it('Should show error text when component has an error', () => {
     const ChildComponent = () => null;
-    const component = mount(
+    const component = shallow(
       <ErrorBoundary>
         <ChildComponent />
       </ErrorBoundary>
     );
-    const error = new Error();
-    component.find(ChildComponent).simulateError(error);
+    component.find(ChildComponent).simulateError(new Error());
     expect(component.state()).toEqual({ hasError: true });
     expect(component.text()).toEqual(
       'Oops!!! Something went wrong please reload the page'
     );
   });
-  
+
   it('Should work as expected without errors', () => {
     const component = shallow(
       <ErrorBoundary>
@@ -34,4 +32,4 @@ describe('<ErrorBoundary />', () => {
     expect(component.state()).toEqual({ hasError: false });
     expect(component.text()).toEqual('All good');
   });
-})
+});
