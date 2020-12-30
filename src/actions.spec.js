a857f4f08c57ea486dad75a6e9b2fe55c973bac6
Fortune Ekeruo <legalmody@gmail.com>
2020-12-30 01:01:51 +0100

    add test for the requestRobots action

--- src/actions.spec.js
+++ src/actions.spec.js
@@ -1,18 +1,35 @@
+import configureMockStore from 'redux-mock-store';
+import thunkMiddleware from 'redux-thunk';
+
 import {
   CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED,
 } from './contants';
 
 import * as actions from './actions';
 
 describe('setSearchField Action', () => {
+  const payload = 'testing';
+  const expectedAction = {
+    type: CHANGE_SEARCH_FIELD,
+    payload,
+  }
   it('Should create an action to search robots', () => {
-    const payload = 'testing';
-    const expectedAction = {
-      type: CHANGE_SEARCH_FIELD,
-      payload,
-    }
-
     expect(actions.setSearchField(payload)).toEqual(expectedAction);
   })
 })
 
+describe('requestRobots Action', () => {
+  const mockStore = configureMockStore([thunkMiddleware])();
+  mockStore.dispatch(actions.requestRobots());
+  const [storeAction] = mockStore.getActions();
+  const expectedAction = {
+    type: REQUEST_ROBOTS_PENDING,
+  }
+
+  it('should handle robots pending request', () => {
+    expect(storeAction).toEqual(expectedAction)
+  })
+})
