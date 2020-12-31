de80877d168891d931f2d72b58c66cc5597cb458
Fortune Ekeruo <legalmody@gmail.com>
2020-12-31 17:29:21 +0100

    memoize and convert Loading component to tsx

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,9 @@
-const Loading = () => (
+import { MemoExoticComponent, memo, FC } from 'react';
+
+const Loading: MemoExoticComponent<FC<{}>> = memo(() => (
   <div className="vh-100 dt w-100">
     <h1 className="tc f1 dtc v-mid">Loading...</h1>
   </div>
-);
+));
 
 export default Loading;
