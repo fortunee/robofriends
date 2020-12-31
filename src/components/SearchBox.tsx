74e8fd2cea956de139ebd17380c003bec16c1602
Fortune Ekeruo <legalmody@gmail.com>
2020-12-31 17:45:59 +0100

    rename SearchBox component to tsx

---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,21 @@
+import { memo, MemoExoticComponent } from 'react';
+
+interface SearchBoxProps {
+  searchChange: () => void;
+}
+
+type MemoParam = (props: SearchBoxProps) => JSX.Element;
+
+const SearchBox: MemoExoticComponent<MemoParam> = memo(({ searchChange }) => (
+  <div className="pa2">
+    <input
+      aria-label="Search Robos"
+      type="search"
+      className="pa3 ba b--green"
+      placeholder="Search robo friends"
+      onChange={searchChange}
+    />
+  </div>
+));
+
+export default SearchBox;
