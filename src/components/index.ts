496c9bd1b7b0281ff4fbea2aa0b9968081cd6a7d
Fortune Ekeruo <legalmody@gmail.com>
2021-01-04 05:09:29 +0100

    export all components as named exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,9 @@
 import Card from './Card/Card';
 import CardList from './CardList/CardList';
-import Header from './Header/Header';
-import Loading from './Loading/Loading';
-import Scroll from './Scroll/Scroll';
-import SearchBox from './SearchBox/SearchBox';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+import Header from './Header';
+import Loading from './Loading';
+import Scroll from './Scroll';
+import SearchBox from './SearchBox';
+import ErrorBoundary from './ErrorBoundary';
 
 export { Card, CardList, Header, Loading, Scroll, SearchBox, ErrorBoundary };
