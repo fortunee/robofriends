8804291c22437b7c52caacd15169e4238221c3e1
Fortune Ekeruo <legalmody@gmail.com>
2021-01-06 00:11:25 +0100

    fetch user on the server side with getServerSideProps

--- src/pages/robots/[id].tsx
+++ src/pages/robots/[id].tsx
@@ -1,28 +1,45 @@
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/dist/client/router';
 import { apiGetReq } from '../../api';
 import { ROBOTS_URL } from '../../constants';
-import { Card, Loading } from '../../components';
+import { Card } from '../../components';
 import { RobotProps } from '../../containers/App';
+import { GetServerSideProps } from 'next';
 
-const Robot = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  const [state, setState] = useState<RobotProps>();
-  useEffect(() => {
-    if (id) {
-      (async () => {
-        const data = await apiGetReq(`${ROBOTS_URL}/${id}`);
-        setState(data);
-      })();
-    }
-  }, [id]);
-
-  if (!state) {
-    return <Loading />
+const Robot = (props: { robot?: RobotProps }) => {
+  if (!props.robot) {
+    return (
+      <div className="vh-100 dt w-100">
+        <h1 className="tc f1 dtc v-mid">
+          Error loading robot!! Please Try again...
+        </h1>
+      </div>
+    );
   }
 
-  return <Card {...state} />
+  return (
+    <div className="vh-100 dt w-100">
+      <div className="tc dtc v-mid">
+        <Card {...props.robot} />
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({
+  params,
+  res,
+}) => {
+  try {
+    const id = params?.id;
+    const robot = await apiGetReq(`${ROBOTS_URL}/${id}`);
+    return {
+      props: {
+        robot,
+      },
+    };
+  } catch {
+    res.statusCode = 404;
+    return { props: {} };
+  }
 };
 
 export default Robot;
