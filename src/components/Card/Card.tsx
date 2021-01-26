0b9ba3b62ed826626c27e1c82b6b03aa4779a224
Fortune Ekeruo <legalmody@gmail.com>
2021-01-26 04:47:34 +0100

    use husky flag in package.json

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,21 +1,16 @@
-import React from 'react';
-import { RobotProps } from '../../containers/App';
+import React from "react";
+import { RobotProps } from "../../containers/App";
 
 const Card = ({ id, name, email }: RobotProps) => (
   <div
-    className="tc bg-light-blue dib br3 pa3 ma2 grow shadow-5 grow pointer
+    className="
+    tc bg-light-blue dib br3 pa3 ma2 grow shadow-5 grow pointer
   "
   >
-    <img 
-    
-    src={`https://robohash.org/${id}?size=200x200`} alt="robot" />
+    <img src={`https://robohash.org/${id}?size=200x200`} alt="robot" />
     <div>
-      <h2>{name}
-      
-      </h2>
-      <p>{email}
-      
-      </p>
+      <h2>{name}</h2>
+      <p>{email}</p>
     </div>
   </div>
 );
